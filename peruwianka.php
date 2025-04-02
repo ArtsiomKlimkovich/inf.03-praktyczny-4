908c14d83098f29a5eac8c0da33800902529052c
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2025-04-02 11:15:50 +0200

    Add files via upload

---
+++ peruwianka.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Hodowla świnek morskich</title>
+</head>
+<body>
+    <section class="baner">
+        <h1>Hodowla świnek morskich - zamów świnkowe maluszki</h1>
+    </section>
+
+    <section>
+        <section class="menu">
+            <a href="peruwianka.php">Rasa Peruwianka</a>
+            <a href="american.php">„Rasa American</a>
+            <a href="crested.php">Rasa Crested</a>
+        </section>
+        <section class="prawy">
+            <h3>Poznaj wszystkie rasy świnek morskich</h3>
+                <ol>
+                    <!-- skrypt 1 -->
+                    <?php
+
+                    ?>
+                </ol>
+        </section>
+        <section class="glowny">
+            <img src="peruwianka.jpg" alt="Świnka morska rasy peruwianka">
+            <!-- skrypt 2 -->
+            <?php
+
+            ?>
+            <hr>
+            <h2>Świnki w tym miocie</h2>
+            <!-- Skrypt 3 -->
+            <?php
+
+            ?>
+        </section>
+    </section>
+
+    <section class="stopka">
+        <p>Stronę wykonał: Artsiom Klimkovich</p>
+    </section>
+</body>
+</html>
